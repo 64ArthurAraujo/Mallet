8b031b19c9e9bb86ba75e51b3335991d4a2c47de
64ArthurAraujo <arthur.araujo@tutanota.com>
2022-01-31 07:29:56 -0300

    Create script for completing searches

--- webapp/assets/javascript/auto_complete.js
+++ webapp/assets/javascript/auto_complete.js
@@ -2,17 +2,40 @@ const searchBar = document.getElementById("search-bar")
 
 let result = null;
 
+fetchSearchSuggestions()
+
 function doAutoComplete(typedContent) {
-	fetchSearchSuggestions()
-	console.log(result)
+	
+	console.log(typedContent.target.value.length)
+	
+	if (result != null) {
+		let cachedSearches = result.replace('[', '').replace(']', '').split(',')		
+		
+		if (typedContent.target.value.length > 0) {
+			cachedSearches.forEach((searchEntry) => {
+				if (searchEntry.startsWith(typedContent.target.value)) {
+					showAutoCompleteString( searchEntry )
+				}
+			})
+		} else {
+			showAutoCompleteString("")
+		}
+		
+		console.log(cachedSearches)
+	}
+}
+
+function showAutoCompleteString(searchEntry, typedContent) {
+	let paragraph = document.getElementById("autocomplete-string")
+	
+	paragraph.innerText = searchEntry
 }
 
 function fetchSearchSuggestions() {
 	const url = 'http://localhost:8080/Mallet/SearchCompleter'
 	
 	const xhr = new XMLHttpRequest()
-	xhr.open('GET', url)
-	xhr.responseType = 'json'
+	xhr.open('POST', url)
 	xhr.onload = () => result = xhr.response
 	
 	xhr.send()
